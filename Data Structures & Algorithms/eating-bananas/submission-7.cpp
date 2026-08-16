9a339308d67d61dc83d077504988f8b370d2ab2e
tarunkakinada07 <tarunkakinada07@gmail.com>
2026-08-16 23:49:25 +0530

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int high = *max_element(piles.begin(), piles.end());
+        int low = 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            long long hours = 0;
+
+            for(int i = 0; i < piles.size(); i++){
+                hours += (piles[i]+mid-1)/mid;
+            };
+
+            if (hours <= h)
+            high = mid - 1;
+            else
+            low = mid + 1;
+            
+        };
+        return low;
+    };
+};
